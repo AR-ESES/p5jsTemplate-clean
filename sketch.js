55131f2916b724ff0758665a00548c51a1b1b83a
André Rocha <steam228@gmail.com>
2022-03-23 11:34:29 +0000

    first

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let laranjas;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0)
+
+}
+
+function draw() {
+
+	stroke(230);
+	strokeWeight(2);
+	circle(width / 2, height / 2, height / 4);
+
+
+}
